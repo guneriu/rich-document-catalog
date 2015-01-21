015b3be9bcdf4078665350317aff62d4f1001efd
guneriu <guneriu@gmail.com>
2015-01-22 01:00:54 +0200

    added spring-loaded for hot-swap

--- src/main/java/org/guneriu/richdocumentcatalog/Application.java
+++ src/main/java/org/guneriu/richdocumentcatalog/Application.java
@@ -1,8 +1,6 @@
 package org.guneriu.richdocumentcatalog;
 
 import org.guneriu.richdocumentcatalog.config.SearchContext;
-import org.guneriu.richdocumentcatalog.model.book.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -10,9 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import({SearchContext.class })
 public class Application {
-	
-	@Autowired
-	BookService bookService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
