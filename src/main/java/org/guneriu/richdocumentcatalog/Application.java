268cb6b6465b064daed409bd9f28ea7255fba46e
guneriu <guneriu@gmail.com>
2015-01-22 00:49:45 +0200

    deleted solr config files and used @SpringBootApplication annotation

--- src/main/java/org/guneriu/richdocumentcatalog/Application.java
+++ src/main/java/org/guneriu/richdocumentcatalog/Application.java
@@ -4,14 +4,10 @@ import org.guneriu.richdocumentcatalog.config.SearchContext;
 import org.guneriu.richdocumentcatalog.model.book.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 @Import({SearchContext.class })
 public class Application {
 	
